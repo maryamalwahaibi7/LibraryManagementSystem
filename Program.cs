f37d5828346ce9dc641427d70e235a1564865588
“Maryam” <maryammaalwahaibi@gmail.com>
2026-01-11 09:55:27 +0400

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,92 @@
+﻿namespace Library
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            //System Storage
+            string[] BookTitles = new string[100];
+            string[] BookISBNs = new string[100];
+            bool[] BookAvailability = new bool[100];
+            string[] BorrowerNames = new string[100];
+            string[] BookAuthors = new string[100];
+            int LastBookIndex = -1;
+
+            //seed data
+            BookTitles[0] = "Math";
+            BookISBNs[0] = "A101";
+            BookAvailability[0] = true;
+            BorrowerNames[0] = "Sara";
+            BookAuthors[0] = "Ali";
+            LastBookIndex++;
+
+            BookTitles[1] = "ComputerNetworks";
+            BookISBNs[1] = "A202";
+            BookAvailability[1] = true;
+            BorrowerNames[1] = "Fatma";
+            BookAuthors[1] = "Ahmed";
+            LastBookIndex++;
+
+
+
+
+
+
+
+
+
+
+
+
+
+        
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+        }
+    }
+}
